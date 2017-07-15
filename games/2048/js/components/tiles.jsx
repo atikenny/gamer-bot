c475dca201e001bddf82d5f21ce62437e8633870
Attila Bartha <atikenny@gmail.com>
2017-07-15 20:25:08 +0100

    add more tile colors

--- games/2048/js/components/tiles.jsx
+++ games/2048/js/components/tiles.jsx
@@ -9,10 +9,17 @@ const Tiles = ({ board }) => (
             row.map((number, columnIndex) => {
                 const rowNumber = rowIndex + 1;
                 const columnNumber = columnIndex + 1;
+                let tileClassName = `tile row-${rowNumber} column-${columnNumber}`;
+
+                if (number > 2048) {
+                    tileClassName += ' tile-super';
+                } else {
+                    tileClassName += ` tile-${number}`;
+                }
 
                 if (number) {
                     return (
-                        <span className={`tile tile-${number} row-${rowNumber} column-${columnNumber}`}>
+                        <span className={tileClassName}>
                             {number}
                         </span>
                     );
