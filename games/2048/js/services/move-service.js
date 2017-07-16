d17dbbb1538a19af6f71b4f7756fd5aa5f9d05fd
Attila Bartha <atikenny@gmail.com>
2017-07-16 14:26:53 +0100

    fix adding twice with different numbers

--- games/2048/js/services/move-service.js
+++ games/2048/js/services/move-service.js
@@ -20,6 +20,27 @@ const getDistinctNumbers = (numbers) => {
     }, []);
 };
 
+const countItemOccurances = (needle, haystack) => {
+    return haystack.reduce((occurances, item) => {
+        return occurances += item === needle ? 1 : 0;
+    }, 0);
+};
+
+const checkHasTwoPairOfNumbers = (numbers, distinctNumbers) => {
+    const occurances = distinctNumbers.map(distinctNumber => countItemOccurances(distinctNumber, numbers));
+    const distinctOccurances = getDistinctNumbers(occurances);
+
+    return distinctOccurances.length === 1;
+};
+
+const checkCanAddTwice = (numbers) => {
+    const distinctNumbers = getDistinctNumbers(numbers);
+    const hasOnlyOneDistinctNumber = distinctNumbers.length === 1;
+    const hasTwoPairOfNumbers = checkHasTwoPairOfNumbers(numbers, distinctNumbers);
+
+    return hasOnlyOneDistinctNumber || hasTwoPairOfNumbers;
+};
+
 export const move = (direction, board) => {
     let columns, newTiles, couldMove = false, summed = 0;
 
@@ -31,8 +52,9 @@ export const move = (direction, board) => {
 
     columns.forEach(column => {
         let summedOnce = false;
+        let alreadySummedIndexes = [];
         const distinctNumbers = getDistinctNumbers(column);
-        const canAddTwice = distinctNumbers.length === 1;
+        const canAddTwice = checkCanAddTwice(column);
 
         if (direction === 'DOWN' || direction === 'RIGHT') {
             column.reverse();
@@ -41,18 +63,21 @@ export const move = (direction, board) => {
         column.forEach((number, rowIndex) => {
             if (number !== 0) {
                 while (--rowIndex >= 0) {
+                    const isAlreadySummedIndex = alreadySummedIndexes.indexOf(rowIndex) !== -1;
+
                     if (column[rowIndex] === 0) {
                         column[rowIndex] = number;
                         column[rowIndex + 1] = 0;
 
                         couldMove = true;
-                    } else if (column[rowIndex] === number && (!summedOnce || canAddTwice)) {
+                    } else if (!isAlreadySummedIndex && (column[rowIndex] === number) && (!summedOnce || canAddTwice)) {
                         column[rowIndex] = 2 * number;
                         column[rowIndex + 1] = 0;
 
                         summedOnce = true;
                         summed += 2 * number;
                         couldMove = true;
+                        alreadySummedIndexes.push(rowIndex);
                     } else {
                         break;
                     }
--- games/2048/js/services/move-service.spec.js
+++ games/2048/js/services/move-service.spec.js
@@ -55,6 +55,40 @@ describe('move method', () => {
         ]);
     });
 
+    test('moves tiles up adding twice when different numbers', () => {
+        const tiles = [
+            [4, 0, 0, 0],
+            [4, 0, 0, 0],
+            [2, 0, 0, 0],
+            [2, 0, 0, 0]
+        ];
+
+        const newBoard = move('UP', { tiles });
+
+        expect(newBoard.tiles).toEqual([
+            [8, 0, 0, 0],
+            [4, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ]);
+
+        const tilesUpsideDown = [
+            [2, 0, 0, 0],
+            [2, 0, 0, 0],
+            [4, 0, 0, 0],
+            [4, 0, 0, 0]
+        ];
+
+        const newBoardUpsideDown = move('UP', { tiles: tilesUpsideDown });
+
+        expect(newBoardUpsideDown.tiles).toEqual([
+            [4, 0, 0, 0],
+            [8, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ]);
+    });
+
     test('moves tiles up adding numbers once with full row', () => {
         const tiles = [
             [2, 0, 0, 0],
