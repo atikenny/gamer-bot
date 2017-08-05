42ad70d6989ff97ae717dbe765caae6ef647da53
Attila Bartha <atikenny@gmail.com>
2017-08-05 21:29:30 +0100

    add transition for tiles, calculate transform in JS

--- games/2048/js/components/tiles.jsx
+++ games/2048/js/components/tiles.jsx
@@ -3,13 +3,23 @@ import { connect }  from 'react-redux';
 
 import 'game2048/styles/components/tiles';
 
+const gutterSize = 12;
+const cellSize = 100;
+const getTransform = (rowIndex, columnIndex) => {
+    const x = (columnIndex * cellSize) + (columnIndex * gutterSize);
+    const y = (rowIndex * cellSize) + (rowIndex * gutterSize);
+
+    return {
+        transform: `translate(${x}px, ${y}px)`
+    };
+};
+
 const Tiles = ({ tiles }) => (
     <div id="tiles">
         {tiles.map((row, rowIndex) => (
             row.map((number, columnIndex) => {
-                const rowNumber = rowIndex + 1;
-                const columnNumber = columnIndex + 1;
-                let tileClassName = `tile row-${rowNumber} column-${columnNumber}`;
+                let tileClassName = `tile`;
+                const style = getTransform(rowIndex, columnIndex);
 
                 if (number > 2048) {
                     tileClassName += ' tile-super';
@@ -19,7 +29,9 @@ const Tiles = ({ tiles }) => (
 
                 if (number) {
                     return (
-                        <span className={tileClassName}>
+                        <span
+                            className={tileClassName}
+                            style={style}>
                             {number}
                         </span>
                     );
